ab88f8eb717d7dc4c6dd6e8ee28f73c295f60c4c
Anna Rankin <anna@annarankin.com>
2016-01-04 16:19:01 -0500

    adds multiplication and division

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -6,4 +6,10 @@ calc.add = function(x,y) {
 calc.subtract = function(x,y) {
   return x - y;
 };
+calc.divide = function(x,y) {
+  return x / y;
+};
+calc.multiply = function(x,y) {
+  return x * y;
+};
 module.exports = calc;
